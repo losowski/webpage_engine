36306083cf2ebfcd38ff7fedd423a247d243ccd7
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2017-06-26 23:56:11 +0300

    Added proper initialisers for base class

--- cpp/html/form/html_form_input.cpp
+++ cpp/html/form/html_form_input.cpp
@@ -11,7 +11,7 @@ HTMLFormInput::HTMLFormInput()
 {
 }
 
-HTMLFormInput::HTMLFormInput(const string & field_name, const string & label, const string & value)
+HTMLFormInput::HTMLFormInput(const string & field_name, const string & label, const string & value): m_field_name(field_name), m_label(label), m_value(value)
 {
 }
 
