7e5712851e7ff34150e1b9e293010b7063b565c1
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2017-10-19 02:08:50 +0300

    libhtml test app build

--- cpp/testing/libhtml/libhtml.cpp
+++ cpp/testing/libhtml/libhtml.cpp
@@ -7,17 +7,19 @@ using namespace html;
 int main (int argc, char * argv[])
 {
 	//Demo Function
-	HTMLPage page ("Hello World");
-	page.add_script("demo");
-	page.add_style("gangnam");
-	page.add_meta_data(header::HTMLHeaderMeta::HTML_META_KEYWORDS, "Demo, Super");
-	HTMLElementFactory.
-	HTMLFormPtr form = page.add_form("testing_form", "web01");
+	HTMLPagePtr page = HTMLElementFactory::create_page("Hello World");
+	page->add_script("demo");
+	page->add_style("gangnam");
+	page->add_meta_data(header::HTMLHeaderMeta::HTML_META_KEYWORDS, "Demo, Super");
+	HTMLElementFactory::add_div(page, "divider1");
+	HTMLElementFactory::add_div(page, "divider1");
+	HTMLFormPtr form = page->add_form("testing_form", "web01");
 	form->add_password_input("pword", "Secret Word");
 	form->add_text_input("nickname", "Pet Name");
 	form->add_text_input("realname", "Full Name", "Dictator Otato");
 	form->add_radio_input("gender", "Male", "male");
 	form->add_radio_input("gender", "Female", "female");
+	HTMLElementFactory::add_h1(form, "divider2", "Animals");
 	HTMLFormInputMenuPtr menu = form->add_menu_input("animals", "Select an Animal");
 	menu->add_menu_selection("Cat", "cat");
 	menu->add_menu_selection("Dog", "dog", false);
@@ -25,7 +27,7 @@ int main (int argc, char * argv[])
 	menu->add_menu_selection("Unicorn", "Unicorn");
 	form->add_reset_input();
 	//form->add_button();
-	page.create_page();
-	std::cout << page.get_page() << std::endl;
+	page->create_html();
+	std::cout << page->get_html() << std::endl;
 }
 
