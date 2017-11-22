d79cf601e8893177515d762b15f76b644a77901d
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2017-11-22 16:06:18 +0200

    Added means to create the database

--- database/create_cmsdb_script.sh
+++ database/create_cmsdb_script.sh
@@ -3,23 +3,23 @@
 # Script to read back backups of the database
 #
 #Create a blank file
-echo "" > create_tradedb.sql
+echo "" > create_cmsdb.sql
 
 #Setup the basics of the database
-cat cms_user.sql >> create_tradedb.sql
-cat cms_database.sql >> create_tradedb.sql
-cat cms_grant.sql >> create_tradedb.sql
+cat cms_user.sql >> create_cmsdb.sql
+cat cms_database.sql >> create_cmsdb.sql
+cat cms_grant.sql >> create_cmsdb.sql
 
 #Setup Database and schema
 # Triple backslash to work with debian (ignores arguments)
-echo "-- SCRIPT BEGIN: set up connection and schema" >> create_tradedb.sql
-echo "\\\connect cmsdb" >> create_tradedb.sql
-echo "set schema 'cms_schema';" >> create_tradedb.sql
-echo "-- SCRIPT END" >> create_tradedb.sql
+echo "-- SCRIPT BEGIN: set up connection and schema" >> create_cmsdb.sql
+echo "\\\connect cmsdb" >> create_cmsdb.sql
+echo "set schema 'cms_schema';" >> create_cmsdb.sql
+echo "-- SCRIPT END" >> create_cmsdb.sql
 
 #Write the data into the system
-cat cmsdb.sql >> create_tradedb.sql
-cat cmsdb_data.sql >> create_tradedb.sql
+cat cmsdb.sql >> create_cmsdb.sql
+cat cmsdb_data.sql >> create_cmsdb.sql
 
 #Modify the schema so we load the correct one
-#sed -iv 's/cms_schema/cmsdb.cms_schema/' create_tradedb.sql
+#sed -iv 's/cms_schema/cmsdb.cms_schema/' create_cmsdb.sql
