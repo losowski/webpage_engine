b7102222cc8a63fbb93f0b016bed1b9f1b7380a5
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2017-11-22 16:10:27 +0200

    Added means to dump the CMS database

---
+++ database/dump_database.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#psql -d cmsdb -U cms -w
+#Script to dump everything
+#pg_dump Ft  -Ucms cmsdb > sql/cmsdb.tar
+#Script to make entire DBi schema, create etc
+pg_dump -svc --host=localhost -Ucms cmsdb > sql/cmsdb.sql
+#Script to store data only
+pg_dump -va --host=localhost --data-only -Ucms cmsdb > sql/cmsdb_data.sql
+#Compress the data
+tar -cvf sql/cmsdb.tar sql/cmsdb.sql sql/cmsdb_data.sql
+#Exchange and Symbols only
+pg_dump -va --host=localhost -Ucms cmsdb --data-only --table=cms_schema.symbol --table=cms_schema.exchange > sql/cmsdb_symbols.sql
