d1bfd27e86fe5fe67dbf1cbca2b7d71927c0dcd4
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2017-12-02 00:23:11 +0200

    Corrected SQL creation scripts to include schema

--- database/cms_database.sql
+++ database/cms_database.sql
@@ -2,3 +2,7 @@
 -- Create the database
 --
 CREATE DATABASE cmsdb OWNER cms;
+--
+-- Create the schema
+--
+CREATE SCHEMA IF NOT EXISTS "cms_schema" AUTHORIZATION cms;
--- database/cmsdb.sql
+++ database/cmsdb.sql
@@ -1,6 +1,6 @@
--- Table: "tCMSSite"
--- DROP TABLE "tCMSSite";
-CREATE TABLE "tCMSSite"
+-- Table: "cms_schema.tCMSSite"
+-- DROP TABLE "cms_schema.tCMSSite";
+CREATE TABLE "cms_schema.tCMSSite"
 (
   id bigserial NOT NULL,
   site_name text NOT NULL,
@@ -9,19 +9,19 @@ CREATE TABLE "tCMSSite"
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE "tCMSSite"
+ALTER TABLE "cms_schema.tCMSSite"
   OWNER TO cms;
 
 -- Index: idx_cmssite_id
 -- DROP INDEX idx_cmssite_id;
 CREATE INDEX idx_cmssite_id
-  ON "tCMSSite"
+  ON "cms_schema.tCMSSite"
   USING btree
   (id);
 
--- Table: "tCMSPage"
--- DROP TABLE "tCMSPage";
-CREATE TABLE "tCMSPage"
+-- Table: "cms_schema.tCMSPage"
+-- DROP TABLE "cms_schema.tCMSPage";
+CREATE TABLE "cms_schema.tCMSPage"
 (
   id bigserial NOT NULL,
   page_name text NOT NULL,
@@ -29,41 +29,41 @@ CREATE TABLE "tCMSPage"
   site_id bigint NOT NULL,
   CONSTRAINT pk_cmspage PRIMARY KEY (id),
   CONSTRAINT fk_cmspage_site_id FOREIGN KEY (site_id)
-      REFERENCES "tCMSSite" (id) MATCH SIMPLE
+      REFERENCES "cms_schema.tCMSSite" (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE "tCMSPage"
+ALTER TABLE "cms_schema.tCMSPage"
   OWNER TO cms;
 
 -- Index: idx_cmspage_page_name
 -- DROP INDEX idx_cmspage_page_name;
 CREATE INDEX idx_cmspage_page_name
-  ON "tCMSPage"
+  ON "cms_schema.tCMSPage"
   USING btree
   (page_name COLLATE pg_catalog."default");
 
 -- Index: idx_cmspage_site_id
 -- DROP INDEX idx_cmspage_site_id;
 CREATE INDEX idx_cmspage_site_id
-  ON "tCMSPage"
+  ON "cms_schema.tCMSPage"
   USING btree
   (site_id);
 
 -- Foreign Key: fk_cmspage_site_id
--- ALTER TABLE "tCMSPage" DROP CONSTRAINT fk_cmspage_site_id;
-ALTER TABLE "tCMSPage"
+-- ALTER TABLE "cms_schema.tCMSPage" DROP CONSTRAINT fk_cmspage_site_id;
+ALTER TABLE "cms_schema.tCMSPage"
   ADD CONSTRAINT fk_cmspage_site_id FOREIGN KEY (site_id)
-      REFERENCES "tCMSSite" (id) MATCH SIMPLE
+      REFERENCES "cms_schema.tCMSSite" (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION;
 
--- Table: "tCMSField"
+-- Table: "cms_schema.tCMSField"
 
--- DROP TABLE "tCMSField";
+-- DROP TABLE "cms_schema.tCMSField";
 
-CREATE TABLE "tCMSField"
+CREATE TABLE "cms_schema.tCMSField"
 (
   id bigserial NOT NULL,
   page_id bigint NOT NULL,
@@ -72,25 +72,25 @@ CREATE TABLE "tCMSField"
   column_name text NOT NULL,
   CONSTRAINT pk_cmsfield PRIMARY KEY (id),
   CONSTRAINT fk_field_page FOREIGN KEY (page_id)
-      REFERENCES "tCMSPage" (id) MATCH SIMPLE
+      REFERENCES "cms_schema.tCMSPage" (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE "tCMSField"
+ALTER TABLE "cms_schema.tCMSField"
   OWNER TO postgres;
 
 -- Index: idx_field
 -- DROP INDEX idx_field;
 CREATE INDEX idx_field
-  ON "tCMSField"
+  ON "cms_schema.tCMSField"
   USING btree
   (id);
 
 -- Index: idx_field_page
 -- DROP INDEX idx_field_page;
 CREATE INDEX idx_field_page
-  ON "tCMSField"
+  ON "cms_schema.tCMSField"
   USING btree
   (page_id);
