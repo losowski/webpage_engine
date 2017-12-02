4621ac27bb59dd4c4da81392818e9db8ace4feb4
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2017-12-02 14:48:32 +0200

    Improving the example stored procedure

--- database/storedprocedures/example_stored_procedures.sql
+++ database/storedprocedures/example_stored_procedures.sql
@@ -1,24 +1,24 @@
-﻿-- SELECT
+-- SELECT
 -- INSERT
 CREATE OR REPLACE FUNCTION demo_schema.pInsContact(
-	p_name					demo_schema.tContact.name%TYPE,
-	p_happiness				demo_schema.tContact.happiness%TYPE,
-	p_created_date			demo_schema.tContact.created_date%TYPE default localtimestamp
+	p_forename				demo_schema.tcontact.forename%TYPE,
+	p_happiness				demo_schema.tcontact.happiness%TYPE,
+	p_created_date			demo_schema.tcontact.created_date%TYPE default localtimestamp
 	) RETURNS bigint AS $$
 DECLARE
-	v_id					demo_schema.tContact.id%TYPE;
+	v_id					demo_schema.tcontact.id%TYPE;
 BEGIN
 	-- Insert data
 	INSERT INTO
 		demo_schema.tContact
 		(
-			name,
+			forename,
 			happiness,
 			created_date
 		)
 	VALUES
 		(
-			p_name,
+			p_forename,
 			p_happiness,
 			p_created_date
 		)
@@ -36,27 +36,25 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-﻿-- Update
+-- Update
 CREATE OR REPLACE FUNCTION demo_schema.pUpdContact(
-	p_id					demo_schema.tContact.id%TYPE,
-	p_name					demo_schema.tContact.name%TYPE default NULL,
-	p_happiness				demo_schema.tContact.happiness%TYPE default NULL,
-	p_created_date			demo_schema.tContact.created_date%TYPE default NULL
+	p_id					demo_schema.tcontact.id%TYPE,
+	p_forename				demo_schema.tcontact.forename%TYPE default NULL,
+	p_happiness				demo_schema.tcontact.happiness%TYPE default NULL,
+	p_created_date			demo_schema.tcontact.created_date%TYPE default NULL
 	) RETURNS bigint AS $$
 DECLARE
-	v_id					demo_schema.tContact.id%TYPE;
-	v_name					demo_schema.tContact.name%TYPE;
-	v_happiness				demo_schema.tContact.happiness%TYPE;
-	v_created_date			demo_schema.tContact.created_date%TYPE;
+	v_forename				demo_schema.tcontact.forename%TYPE;
+	v_happiness				demo_schema.tcontact.happiness%TYPE;
+	v_created_date			demo_schema.tcontact.created_date%TYPE;
 BEGIN
 	-- Get current Data and lock it
 	SELECT FOR UPDATE
-		COALESCE(p_name, name),
+		COALESCE(p_forename, forename),
 		COALESCE(p_happiness, happiness),
 		COALESCE(p_created_date, created_date),
 	INTO
-		v_id,
-		v_name,
+		v_forename,
 		v_happiness,
 		v_created_date
 	FROM
@@ -64,19 +62,18 @@ BEGIN
 	WHERE
 		id = p_id
 	;
-
 	-- Update data
 	UPDATE
 		demo_schema.tContact
 	SET
 		(
-			name,
+			forename,
 			happiness,
 			created_date
 		)
 	VALUES
 		(
-			v_name,
+			v_forename,
 			v_happiness,
 			v_created_date
 		)
@@ -88,12 +85,12 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-﻿-- Delete
+-- Delete
 CREATE OR REPLACE FUNCTION demo_schema.pDelContact(
-	p_id					demo_schema.tContact.id%TYPE
+	p_id					demo_schema.tcontact.id%TYPE
 	) RETURNS bigint AS $$
 DECLARE
-	v_id					demo_schema.tContact.id%TYPE;
+	v_id					demo_schema.tcontact.id%TYPE;
 BEGIN
 	-- Delete data
 	DELETE FROM
