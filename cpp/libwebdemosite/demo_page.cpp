b68d529f110608f1f58a55d4914cf602faa86896
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2017-12-23 00:05:08 +0200

    Some debugging for the update

--- cpp/libwebdemosite/demo_page.cpp
+++ cpp/libwebdemosite/demo_page.cpp
@@ -100,12 +100,12 @@ void DemoPage::actionDataSelectSQL (pqxx::work & txn)
 
 void DemoPage::actionDataUpdateSQL (pqxx::work & txn, const string & key)
 {
-	//pqxx::result res = txn.exec("demo_schema.pInsUpdContact(" + txn.quote(m_id) + ","  + txn.quote(m_forename) + ","  + txn.quote(m_happiness) +","  + txn.quote(m_created_date) +")");
-	/*
+	std::cerr << "Key = " << key << std::endl;
+	pqxx::result res = txn.exec("demo_schema.pInsUpdContact(" + txn.quote(m_id) + ","  + txn.quote(m_forename) + ","  + txn.quote(m_happiness) +","  + txn.quote(m_created_date) +")");
 	if (0 == res.affected_rows())
 	{
+		std::cerr << "Stuff changed" << std::endl;
 	}
-	*/
 }
 
 
