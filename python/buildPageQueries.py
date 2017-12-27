1b281238b064638f03a65fe5b7310ea66484f4f3
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2017-12-27 18:26:22 +0200

    Main file for CGI building python script written

---
+++ python/buildPageQueries.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# Autogenerate script to build the CGI classes for a CGI application
+def main():
+	# my code here
+	print "Autogenerate CGI classes"
+
+if __name__ == "__main__":
+	main()
