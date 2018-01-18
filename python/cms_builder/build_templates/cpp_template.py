0aa9733d00714b718520ed21091fefb50a2dbce0
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2018-01-19 01:11:47 +0200

    Cleaned up some bad coding in the template

--- python/cms_builder/build_templates/cpp_template.py
+++ python/cms_builder/build_templates/cpp_template.py
@@ -3,9 +3,9 @@
 from string import Template
 
 class CPPTemplate:
-	def __init__(self, template_file, output_file, datamap):
-		self.m_template_file = template_file
-		self.m_output_file = output_file
+	def __init__(self, templateFile, outputFile, datamap):
+		self.m_templateFile = templateFile
+		self.m_outputFile = outputFile
 		self.m_datamap = datamap
 		#Template Data
 		self.m_template = str()
@@ -16,7 +16,7 @@ class CPPTemplate:
 		pass
 
 	def loadTemplate(self):
-		templateFile = open("templates/" + self.m_template_file, 'r')
+		templateFile = open("templates/" + self.m_templateFile, 'r')
 		contents = templateFile.read()
 		self.m_template = Template(contents)
 		templateFile.close()
@@ -37,7 +37,7 @@ class CPPTemplate:
 
 	def generateSourceCode(self):
 		output = self.m_template.safe_substitute(self.m_datamap)
-		outputFile = open("../cpp/libwebcms/" + self.m_output_file, 'w')
+		outputFile = open("../cpp/libwebcms/" + self.m_outputFile, 'w')
 		outputFile.write(output)
 		outputFile.flush()
 		outputFile.close()
