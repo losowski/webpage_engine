cc22aee3d14486ca1ca20e2553b11f679c4b00ca
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2018-01-25 22:56:48 +0000

    Fixed Variable names

--- python/cms_builder/build_templates/base_template.py
+++ python/cms_builder/build_templates/base_template.py
@@ -11,12 +11,12 @@ class BaseTemplate:
 	RAWDATA_VARIABLE_LIST		=	'VARIABLE_LIST'
 
 	def __init__(self, templateFile, outputFile):
-		self.templateFile = templateFile
+		self.templateFileName = templateFile
 		self.outputFile = outputFile
 		#Template datamap	
 		self.dataMap = dict()
 		#Template contents
-		self.m_template = str()
+		self.templateFileContents = str()
 
 	def __del__(self):
 		#Close the template file
@@ -27,11 +27,11 @@ class BaseTemplate:
 		pass
 
 	def loadTemplate(self):
-		print ("Loading template {templatefile}".format(templatefile = self.templateFile))
-		templateFile = open("templates/" + self.templateFile, 'r')
-		contents = templateFile.read()
-		self.m_template = Template(contents)
-		templateFile.close()
+		print ("Loading template {templateFileName}".format(templateFileName = self.templateFileName))
+		templateFileObject = open("templates/" + self.templateFileName, 'r')
+		contents = templateFileObject.read()
+		self.templateFileContents = Template(contents)
+		templateFileObject.close()
 
 	def generateParameters(self):
 		#Basic Functionality
@@ -49,7 +49,7 @@ class BaseTemplate:
 
 	def generateSourceCode(self):
 		print ("Genearating source code file {outputfile}".format(outputfile = self.outputFile))
-		output = self.m_template.safe_substitute(self.dataMap)
+		output = self.templateFileContents.safe_substitute(self.dataMap)
 		outputFile = open("../" + self.outputFile, 'w')
 		outputFile.write(output)
 		outputFile.flush()
