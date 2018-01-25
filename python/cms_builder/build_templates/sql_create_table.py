7e6bc65655a981dfa6105c411f98788c17f2adac
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2018-01-25 22:05:25 +0000

    SQL files created

---
+++ python/cms_builder/build_templates/sql_create_table.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#Class to deal with the template for a CPP Implementation
+
+#Import relative to executing python file
+import cpp_template
+
+from string import Template
+
+class SQLCreateTable (cpp_template.CPPTemplate):
+	#Headers
+	MAKEFILE_FILES			=	'MAKEFILE_FILES'
+	MAKEFILE_PROGRAM_NAME	=	'MAKEFILE_PROGRAM_NAME'
+
+	def __init__(self, output, binaryName, cgiObjects ):
+		cpp_template.CPPTemplate.__init__(self, "makefile", output)
+		self.binaryName = binaryName
+		self.cgiObjects = cgiObjects
+		pass
+
+	def __del__(self):
+		cpp_template.CPPTemplate.__del__(self)
+
+	def populateDataMap(self):
+		#Overload to support dict objects as variables
+		pass
+
+	def extendSpecificParameters(self):
+		#m_datamap.keys() must be first
+		self.m_datamap[self.MAKEFILE_FILES] = ' '.join(cgiObj.getFileName() + ".cpp" for cgiObj in self.cgiObjects)
+		#Executable MUST be after (order sensitive)
+		self.m_datamap[self.MAKEFILE_PROGRAM_NAME] = self.binaryName
+
+
+
---
+++ python/cms_builder/build_templates/sql_template.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+#Class to deal with the template for a CPP template
+import cpp_template
+
+from string import Template
+
+class SQLTemplate (cpp_template.CPPTemplate):
+	#CONSTANTS FROM TEMPLATES
+	RAWDATA_HEADER_IFDEF		=	'HEADER_IFDEF_HPP'
+	RAWDATA_CLASS_NAME			=	'CLASS_NAME'
+	RAWDATA_CLASS_VARIABLES		=	'CLASS_VARIABLES'
+	PRETTY_VARIABLE_LIST		=	'PRETTY_VARIABLE_LIST'
+
+	def __init__(self, templateFile, outputFile, className, baseClass, dbSchema, dbTableName, formDesign, variableList):
+		cpp_template.CPPTemplate.__init__(self, templateFile, outputFile)
+		self.className = className
+		self.baseClass = baseClass
+		self.dbSchema = dbSchema
+		self.dbTableName = dbTableName
+		self.formDesign = formDesign
+		self.variableList = variableList
+		self.prettyVariableList = None
+		#Populate
+		self.populateDataMap()
+
+	def __del__(self):
+		cpp_template.CPPTemplate.__del__(self)
+		pass
+
+	def populateDataMap(self):
+		self.m_datamap = {	self.RAWDATA_FILENAME 		:	self.className,
+							self.RAWDATA_BASE_CLASS 	:	self.baseClass,
+							self.RAWDATA_DB_SCHEMA 		:	self.dbSchema,
+							self.RAWDATA_TABLE_NAME		:	self.dbTableName,
+							self.RAWDATA_VARIABLE_LIST	:	self.variableList,
+						}
+
+	def generateParametersTemplate(self):
+		#All the specific files to output
+		#BaseClass
+		if (None == self.baseClass):
+			self.m_datamap[self.RAWDATA_BASE_CLASS] = "WebPageBase"
+		#Class name is "class_name"
+		titleClassList = self.className.split('_')
+		titleClassName = ''.join( name.capitalize() for name in titleClassList)
+		#Generators
+		self.m_datamap[self.RAWDATA_CLASS_NAME] = titleClassName #"FileName"
+		self.m_datamap[self.RAWDATA_HEADER_IFDEF] = self.className.upper() + "_HPP" # "FILE_NAME"
+		#Variable list
+		prettyVariableList = list()
+		for variable in self.m_datamap.get(self.RAWDATA_VARIABLE_LIST):
+			splitVariable = variable.split('_')
+			prettyVariable =  ' '.join( name.capitalize() for name in splitVariable)
+			prettyVariableList.append(prettyVariable)
+		self.prettyVariableList = prettyVariableList
+		self.m_datamap[self.PRETTY_VARIABLE_LIST] = prettyVariableList
+		#, self.m_datamap.get(self.PRETTY_VARIABLE_LIST)):
+
+
