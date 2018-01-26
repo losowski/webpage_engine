11fcbff067f24a58e4e2a8d1ec9500c3cd0cd818
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2018-01-26 21:55:13 +0000

    Added SQL variable builder

--- python/cms_builder/build_templates/sql_template.py
+++ python/cms_builder/build_templates/sql_template.py
@@ -20,41 +20,19 @@ class SQLTemplate (base_template.BaseTemplate):
 		self.dbSchema = dbSchema
 		self.dbTableName = dbTableName
 		self.variableList = variableList
-		self.prettyVariableList = None
+		self.sqlVariableList = list()
 		#Populate
+		self.__initialiseVariableLists(variableList)
 		self.populateDataMap()
 
 	def __del__(self):
 		base_template.BaseTemplate.__del__(self)
 		pass
 
-	def populateDataMap(self):
-		self.dataMap = {	self.RAWDATA_FILENAME 		:	self.className,
-							self.RAWDATA_BASE_CLASS 	:	self.baseClass,
-							self.RAWDATA_DB_SCHEMA 		:	self.dbSchema,
-							self.RAWDATA_TABLE_NAME		:	self.dbTableName,
-							self.RAWDATA_VARIABLE_LIST	:	self.variableList,
-						}
-
-	def generateParametersTemplate(self):
-		#All the specific files to output
-		#BaseClass
-		if (None == self.baseClass):
-			self.dataMap[self.RAWDATA_BASE_CLASS] = "WebPageBase"
-		#Class name is "class_name"
-		titleClassList = self.className.split('_')
-		titleClassName = ''.join( name.capitalize() for name in titleClassList)
-		#Generators
-		self.dataMap[self.RAWDATA_CLASS_NAME] = titleClassName #"FileName"
-		self.dataMap[self.RAWDATA_HEADER_IFDEF] = self.className.upper() + "_HPP" # "FILE_NAME"
-		#Variable list
-		prettyVariableList = list()
-		for variable in self.dataMap.get(self.RAWDATA_VARIABLE_LIST):
-			splitVariable = variable.split('_')
-			prettyVariable =  ' '.join( name.capitalize() for name in splitVariable)
-			prettyVariableList.append(prettyVariable)
-		self.prettyVariableList = prettyVariableList
-		self.dataMap[self.PRETTY_VARIABLE_LIST] = prettyVariableList
-		#, self.dataMap.get(self.PRETTY_VARIABLE_LIST)):
+	def __initialiseVariableLists (self, variableList):
+		for var in variableList:
+			declared = "v_" + var
+			param = "p_" + var
+			self.sqlVariableList.append((var, declared, param)) #tuple
 
 
