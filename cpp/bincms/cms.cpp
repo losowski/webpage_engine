f56c42a7c62c3e37043544e78b45ab1c8d461aa7
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2018-02-10 23:18:10 +0000

    FIX: Correct use of pointers

--- cpp/bincms/cms.cpp
+++ cpp/bincms/cms.cpp
@@ -8,12 +8,13 @@ int main (int argc, char * argv[])
 	WebSiteCMS cms("CMS");
 	WebPageBase * page = cms.createObjectFromKey();
 	//CGI
-	page.connectDB("host=localhost dbname=cmsdb user=cms password=cmsdb");
-	page.parseCGI();
-	page.actionData();
+	page->connectDB("host=localhost dbname=cmsdb user=cms password=cmsdb");
+	page->parseCGI();
+	page->actionData();
 	//Build
-	page.buildWebsite();
-	page.displayWebsite();
+	page->buildWebsite();
+	page->displayWebsite();
+	//Cleanup
 	cms.deleteObjectFromKey(page);
 }
 
