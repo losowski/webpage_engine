91993735318f44cf2d0f50009e8703f8088241b0
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2018-02-10 22:42:49 +0000

    FIX: Proper List initialisation

--- python/templates/site.cpp
+++ python/templates/site.cpp
@@ -48,9 +48,9 @@ WebPageBase * WebSiteCMS::deleteObjectFromKey(WebPageBase * pagebase)
 
 void WebSiteCMS::buildMenus(WebPageBase * pagebase)
 {
-	list < MenuItem > itemlist = \
+	list < MenuItem > itemlist {
 $CGI_MENU_ITEMS
-	;
+	};
 	//Add the Menu Entries
 	BOOST_FOREACH(MenuItem item, itemlist)
 	{
